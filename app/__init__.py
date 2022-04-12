1c54df676c52e7563fb6736915a7824452d1fa6d
Becca Elenzil <becca@adadevelopersacademy.org>
2022-04-12 09:41:13 -0700

    update mark_complete, mark_incomplete, goal_tasks_post routes

--- app/__init__.py
+++ app/__init__.py
@@ -30,5 +30,9 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     # Register Blueprints here
+    from .routes import tasks_bp, goals_bp, root_bp
+    app.register_blueprint(tasks_bp)
+    app.register_blueprint(goals_bp)
+    app.register_blueprint(root_bp)
 
     return app
