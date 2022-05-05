628dddb6ad94cc5818c863354de64d9b9b709f5d
thaimynguyen <thaimynguyen@gmail.com>
2022-05-05 15:42:28 -0700

    Add logic to routes /tasks GET to get all tasks & pass first 2 tests in Wave 1

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,22 @@ from app import db
 
 
 class Task(db.Model):
+
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    @property
+    def to_JSON_response(self):
+        payload = {
+            "task": {
+                "id": self.task_id,
+                "title": self.title,
+                "description": self.description,
+                "is_complete": False
+            }
+        }
+        if self.completed_at:
+            payload["task"]["is_complete"] = True
+        return payload
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -3,3 +3,11 @@ from app.models.task import Task
 from flask import Blueprint, jsonify, abort, make_response, request
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+
+@ tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks_response = []
+    for task in Task.query:
+        tasks_response.append(task.to_JSON_response["task"])
+    return jsonify(tasks_response), 200
