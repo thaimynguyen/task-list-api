7ecea7e34df96fab7b56ecb870bcda3d2dbe5103
thaimynguyen <thaimynguyen@gmail.com>
2022-05-05 22:51:58 -0700

    Create function send_notifcation that calls Slack API and send a message to channel task-notifcations

---
+++ app/routes/utils/slack_bot.py
@@ -0,0 +1,38 @@
+import requests
+import time
+import json
+import os
+
+
+def send_notification(text):
+
+    url = "https://slack.com/api/chat.postMessage"
+
+    SLACK_API_KEY = os.environ.get("SLACK_API_KEY")
+
+    payload = {
+        "channel": "task-notifications",
+        "text": text}
+
+    headers = {
+        "content-type": "application/json",
+        "Authorization": f"Bearer {SLACK_API_KEY}"}
+
+    # to avoid API rate limit
+    retry_count = 0
+    max_retries = 5
+    wait_time = 2
+
+    while retry_count <= max_retries:
+        response = requests.post(
+            url, data=json.dumps(payload), headers=headers)
+        if response.status_code == 200:
+            return response
+        elif response.status_code == 429:
+            time.sleep(wait_time)
+            retry_count += 1
+            wait_time *= 2
+        else:
+            pass
+
+    return response
