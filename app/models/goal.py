2837d39550a343baf95da13b77e4fd5465cb85dd
thaimynguyen <thaimynguyen@gmail.com>
2022-05-06 08:31:48 -0700

    Update Goal model schema

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,4 +2,5 @@ from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
