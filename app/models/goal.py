fbfe7eca840fa8131e29022e125c5fc99bb36815
thaimynguyen <thaimynguyen@gmail.com>
2022-05-06 08:36:26 -0700

    Add to_dict() instance method to class Goal

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,11 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "goal": {
+                "id": self.goal_id,
+                "title": self.title
+            }
+        }
