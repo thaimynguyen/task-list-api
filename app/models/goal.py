096210e3960083a69905976e4ce0fb36406e2256
thaimynguyen <thaimynguyen@gmail.com>
2022-05-06 12:19:01 -0700

    Add 1-to-many relationship for Task & Goal models

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,8 +2,10 @@ from app import db
 
 
 class Goal(db.Model):
+
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String, unique=True, nullable=False)
+    tasks = db.relationship("Task", back_populates="goal", lazy=True)
 
     def to_dict(self):
         return {
--- app/models/task.py
+++ app/models/task.py
@@ -3,10 +3,13 @@ from app import db
 
 class Task(db.Model):
 
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable=True)
+    goal = db.relationship("Goal", back_populates="tasks", lazy=True)
+
 
     def to_dict(self):
         payload = {
@@ -19,4 +22,7 @@ class Task(db.Model):
         }
         if self.completed_at:
             payload["task"]["is_complete"] = True
+        if self.goal_id:
+            payload["task"]["goal_id"] = self.goal_id
         return payload
+
